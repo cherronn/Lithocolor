c90c58aabe396453591170ffff579cdd57513ae3
cherronn <166733423+cherronn@users.noreply.github.com>
2026-02-20 13:11:06 -0600

    Use environment variable for server port

--- server.py
+++ server.py
@@ -106,4 +106,6 @@ def download_histogram():
     )
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=8000, debug=False)
+    import os
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port, debug=False)
